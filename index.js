37a70d9d909208e23255ce9a9ee1c3238188fdaa
Ruslan0010 <137960337+Ruslan0010@users.noreply.github.com>
2025-05-20 16:58:24 -0400

    Register Validation

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
+
 mongoose
 .connect('mongodb+srv://ruslan0010:Z0PuoQlMDjPSFcRd@cluster0.xhabo44.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('DB ok'))
@@ -11,29 +14,17 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-  res.send('Hello World');
-});
-
-app.post('/auth/login', (req, res) =>{
-    console.log(req.body);
-
-
-if (req.body.email == 'test@test.com'){
-    const token = jwt.sign(
-    {
-    email: req.body.email,
-    fullName: 'Ruslan Kuchynskyi',
-    }, 
-    'secret123',
-);
-}
+app.post('/auth/register', registerValidation, (req, res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json(errors.array);
+    }
 
     res.json({
         success: true,
-        token,
     });
 });
+
 
 app.listen(4444, (err) =>{
     if (err){
